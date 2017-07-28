8ce093127ca47d59abc008d711787849da19a404
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-28 23:50:38 +0430

    Adds basics of arrays

---
+++ session-1/arrays/index.js
@@ -0,0 +1,15 @@
+// Creating simple array
+let fruits = ['Apple', 'Banana'];
+console.log(fruits.length);
+// Getting items
+let first = fruits[0];
+// Apple
+let last = fruits[fruits.length - 1];
+// Banana
+let newLength = fruits.push('Orange');
+// ["Apple", "Banana", "Orange"]
+let last = fruits.pop(); // remove Orange (from the end)
+// ["Apple", "Banana"];
+
+// this is how to remove an item
+let removedItem = fruits.splice(pos, 1);
