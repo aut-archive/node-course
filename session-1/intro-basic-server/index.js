e2f15869b455b5baeb75c8c4aaf74a0360a7e690
Pooya Parsa <pooya@pi0.ir>
2017-07-28 22:49:19 +0430

    fix eslint

--- session-1/intro-basic-server/index.js
+++ session-1/intro-basic-server/index.js
@@ -1,15 +1,15 @@
-const http = require('http');
+const http = require('http')
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'
 
-const port = 3000;
+const port = 3000
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
+})
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
