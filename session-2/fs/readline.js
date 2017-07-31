a308e80466d59bcb2ab073af90074ad0038f95f7
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-31 07:36:26 +0430

    Improves readline significantly

--- session-2/fs/readline.js
+++ session-2/fs/readline.js
@@ -8,6 +8,16 @@ const rl = readline.createInterface({
 rl.question('What do you think of Node.js? ', (answer) => {
   // TODO: Log the answer in a database
   console.log(`Thank you for your valuable feedback: ${answer}`)
-
-  rl.close()
 })
+
+// let prefix = 'OHAI> '
+// rl.setPrompt(prefix, prefix.length)
+// rl.prompt()
+
+// rl.on('line', (input) => {
+//  console.log(`Received: ${input}`)
+// })
+
+rl.write('Delete this!')
+// Simulate Ctrl+u to delete the line written previously
+// rl.write(null, { ctrl: true, name: 'u' })
