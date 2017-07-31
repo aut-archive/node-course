373d8fee0f48c73de61a9d7400a7676f602e52b6
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-31 07:41:06 +0430

    Improves fs example

--- session-2/fs/fs.js
+++ session-2/fs/fs.js
@@ -14,3 +14,32 @@ fs.stat('/tmp/world', (err, stats) => {
   if (err) throw err
   console.log(`stats: ${JSON.stringify(stats)}`)
 })
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err
+  console.log('Saved!')
+})
+
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+  if (err) throw err
+  console.log('Saved!')
+})
+
+fs.readFile('demofile1.html', function (err, data) {
+  if (err) {
+    throw Error('What?!')
+  }
+  console.log(data)
+})
+
+let http = require('http')
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function (err, data) {
+    if (err) {
+      throw Error('Really?!')
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+}).listen(8080)
