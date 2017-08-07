a0fb262d660cb39c6f4ccec8a4bb69401247076c
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-07 09:47:44 +0430

    Removes some blank spaces

--- session-3/web-intro/1.http.js
+++ session-3/web-intro/1.http.js
@@ -3,18 +3,18 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  
+
   const { headers, url, method } = req
 
   if(url === '/test') {
     res.end(JSON.stringify({url, method, headers}))
   } else if(url === '/') {
-    res.end('Hello!') 
+    res.end('Hello!')
   } else {
     res.statusCode = 404
     res.end('404!')
   }
-  
+
 })
 
 server.listen(3001)
--- session-3/web-intro/2.connect.js
+++ session-3/web-intro/2.connect.js
@@ -6,7 +6,7 @@ const STYLE = '<style> * { font-size: 100px; } </style><body>'
 
 app.use((req, res, next) => {
     console.log(req.url)
-    
+
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     res.write(STYLE)
